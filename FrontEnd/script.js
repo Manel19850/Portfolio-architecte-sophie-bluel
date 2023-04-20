e0c5055a72fa4810064a6115c751919807100edb
Manel Jouini <manl.j@hotmail.fr>
2023-04-20 18:28:54 +0200

    Filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -9,11 +9,12 @@
     const article = product[i];
     const imageUrl= article.imageUrl;
     const title = article.title;
-    
+    console.log (product);
+
     const card = document.createElement ("div")
 
     const imageUrlElement = document.createElement("img");
-    imageUrlElement.src =imageUrl;
+    imageUrlElement.src = imageUrl;
     
     const titleElement = document.createElement("p");
     titleElement.innerText= title ;
@@ -27,3 +28,33 @@
     })
 
 
+fetch ("http://localhost:5678/api/categories")
+.then ((reponse) =>reponse.json())
+.then ((categories) => {
+ 
+    
+
+const BoutonAll = document.querySelector("#btn-1");
+const BoutonObjets= document.querySelector("#btn-2");
+const BoutonAppartements= document.querySelector("#btn-3");
+const BoutonRestauEtHotel= document.querySelector("#btn-4");
+
+//aficher tous les élements//
+
+
+
+//afficher les objets//
+
+BoutonObjets.addEventListener("click", (e)=>{
+ console.log(e)   });
+    
+//afficher les appartements//
+BoutonAppartements.addEventListener("click", ()=>{
+
+    });
+//afficher les restau et Hotel//
+
+BoutonRestauEtHotel.addEventListener("click", ()=>{
+
+});
+});
