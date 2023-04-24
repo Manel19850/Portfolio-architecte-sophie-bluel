2eb602655ab07efa3171b4d86e06c8997570c68d
Manel Jouini <manl.j@hotmail.fr>
2023-04-24 14:11:09 +0200

    MAJ des filtres  et de la partie Login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,31 +1,30 @@
-const container = document.getElementById ("container")
-const loginEmail = document.getElementById ("email")
-const loginMotDePasse= document.getElementById ("motDePasse")
-const butonConex = document.getElementById ("valider")
 
-butonConex.addEventListener ("click", function (){
-if (loginEmail.value-length <1 || loginMotDePasse.value-length<1){
+const loginEmail = document.getElementById("email")
+const loginMotDePasse= document.getElementById("motDePasse")
+const butonConex = document.getElementById("valider")
+console.log(butonConex);
+butonConex.addEventListener("click", function (){
+if (loginEmail.value.length == 0 || loginMotDePasse.value.length == 0){
     alert('ERROR'); return
 } 
-
-fetch ("http://localhost:5678/api/users/login",{
-method:"POST",
-
-headers: {"Content-Type":"application/json"},
-
-body:JSON.stringify({
-    email: loginEmail.value,
-    password: loginMotDePasse.value
+console.log(loginEmail.value);
+console.log (loginMotDePasse.value);
+  fetch ("http://localhost:5678/api/users/login",{
+    method:"POST",
+    headers: {"Content-Type":"application/json"},
+    body:JSON.stringify({   
+      email: loginEmail.value,
+      password: loginMotDePasse.value
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log (data);
+      if (data.success) {
+        window.location.replace("index.html");
+      }
+    })
   })
 })
-})
-.then(response => response.json())
-.then(data => {
-  if (data.success) {
-    window.location.replace("file:///C:/Users/manel.jouini.ONCEFORALL/OneDrive%20-%20ALG/Bureau/Projet_Sophie_Bluel/Portfolio-architecte-sophie-bluel/FrontEnd/index.html");
-  }
-})
-
 
 
 
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,67 +1,85 @@
+fetch("http://localhost:5678/api/works")
+    .then((reponse) => reponse.json())
+    .then((product) => {
+        console.log(product);
 
-    fetch("http://localhost:5678/api/works")
-    .then ((reponse) => reponse.json())
-    .then ((product) => {
-        console.log (product);
-
-        for (let i = 0; i < product.length; i++)
+        function AfficherProjet(product) {
+            
+            for (let i = 0; i < product.length; i++)
 
 
-{  
-    const article = product[i];
-    const imageUrl= article.imageUrl;
-    const title = article.title;
-
-    const card = document.createElement ("div");
-
-    const imageUrlElement = document.createElement("img");
-    imageUrlElement.src = imageUrl;
+            {
+                const article = product[i];
+                const imageUrl = article.imageUrl;
+                const title = article.title;
     
-    const titleElement = document.createElement("p");
-    titleElement.innerText=title ;
+                const card = document.createElement("div");
     
-    sectionGallery = document.querySelector (".gallery")
-    card.appendChild(imageUrlElement);
-    card.appendChild(titleElement);
-    sectionGallery.appendChild(card);
-
-}})
-
-//aficher tous les élements//
-
-fetch ("http://localhost:5678/api/categories")
-.then ((reponse) => reponse.json())
-.then ((product) => {
-
-const BoutonAll = document.querySelector("#btn-1");
-console.log(BoutonAll);
-BoutonAll.addEventListener("click",() => { 
-const projetFiltre = product.filter ((categoryId) => { return categoryId ===1; }) ;
-console.log (projetFiltre);
+                const imageUrlElement = document.createElement("img");
+                imageUrlElement.src = imageUrl;
     
-})
+                const titleElement = document.createElement("p");
+                titleElement.innerText = title;
+    
+                sectionGallery = document.querySelector(".gallery")
+                card.appendChild(imageUrlElement);
+                card.appendChild(titleElement);
+                sectionGallery.appendChild(card);
+    
+            }
+        }
+        
+        AfficherProjet(product);
+
+        const BoutonAll = document.querySelector("#All");
+        console.log(BoutonAll);
+        BoutonAll.addEventListener("click", () => {
+            const projetFiltre = product.filter(() => {
+                return product;
+            });
+            sectionGallery = document.querySelector(".gallery");
+            sectionGallery.innerHTML=""
+            AfficherProjet(projetFiltre);
+            console.log(projetFiltre);
+
+        })
 
 
-const BoutonObjets = document.querySelector("#btn-2");
-console.log(BoutonObjets);
-BoutonObjets.addEventListener("click",() => {
-const projetFiltre = product.filter ((categoryId) => {return categoryId === 2;}) ;
-console.log (projetFiltre);
-})
+        const BoutonObjet = document.querySelector("#Objets");
+        console.log(BoutonObjet);
+        BoutonObjet.addEventListener("click", () => {
+            const projetFiltre = product.filter((oneproduct) => {
+                return oneproduct.categoryId === 1;
+            });
+            sectionGallery = document.querySelector(".gallery");
+            sectionGallery.innerHTML=""
+            AfficherProjet(projetFiltre);
+            console.log(projetFiltre);
+        })
 
-const BoutonAppartements = document.querySelector("#btn-3");
-console.log(BoutonAppartements);
-BoutonAppartements.addEventListener("click",() => {
-const projetFiltre = product.filter ((categoryId) => {return categoryId === 3;}) ;
-console.log (projetFiltre);
+        const BoutonAppartements = document.querySelector("#Appartements");
+        console.log(BoutonAppartements);
+        BoutonAppartements.addEventListener("click", () => {
+            const projetFiltre = product.filter((oneproduct) => {
+                return oneproduct.categoryId === 2;
+            });
+            sectionGallery = document.querySelector(".gallery");
+            sectionGallery.innerHTML=""
+            AfficherProjet(projetFiltre);
+            console.log(projetFiltre);
 
-})
+        })
 
-const BoutonRestauEtHotel = document.querySelector("#btn-4");
-console.log(BoutonRestauEtHotel);
-BoutonRestauEtHotel.addEventListener("click",() => {
-const projetFiltre = product.filter ((categoryId) => {return categoryId ===4}) ;
-console.log (projetFiltre); 
-})
+        const BoutonHotelEtRestau = document.querySelector("#HotelEtRestau");
+        console.log(BoutonHotelEtRestau);
+        BoutonHotelEtRestau.addEventListener("click", () => {
+            const projetFiltre = product.filter((oneproduct) => {
+                return oneproduct.categoryId === 3
+            });
+            sectionGallery = document.querySelector(".gallery");
+            sectionGallery.innerHTML=""
+            AfficherProjet(projetFiltre);
+            console.log(projetFiltre);
+        })
 
-})
+    })
